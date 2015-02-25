c4b533f855458c8b56d756c5cab1f8d94a17a79a
Jess Ochs-Willard <jochs@brandeis.edu>
2015-02-25 01:13:47 -0500

    migrated featured artist scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225052532) do
+ActiveRecord::Schema.define(version: 20150225061259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20150225052532) do
     t.string   "artist_id"
     t.string   "genre"
     t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "featured_artists", force: :cascade do |t|
+    t.string   "artist_id"
+    t.string   "song_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

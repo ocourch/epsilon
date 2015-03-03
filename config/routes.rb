f5f50102508d870a1060090a8e6172c48d804382
Oscar Courchaine <oscar.courchaine@gmail.com>
2015-03-02 19:46:10 -0500

    added bootstrap to stations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "stations/index"
   resources :reviews
 
   devise_for :users
@@ -18,6 +19,7 @@ Rails.application.routes.draw do
 
   resources :songs
 
+  root 'stations#index', as: 'sta'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

008c671554eab14810df3f673483a18d4193e1ac
Jess Ochs-Willard <jochs@brandeis.edu>
2015-03-04 15:43:23 -0500

    added name column to album

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150304191610) do
+ActiveRecord::Schema.define(version: 20150304204200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150304191610) do
     t.datetime "updated_at",   null: false
     t.string   "location"
     t.boolean  "in_lib"
+    t.string   "name"
   end
 
   create_table "artists", force: :cascade do |t|

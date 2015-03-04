e5e352cb193e9c776022fed2f162df0d611755c9
Jess Ochs-Willard <jochs@brandeis.edu>
2015-03-04 03:58:35 -0500

    changed root in routes to just stations#index

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
 
   resources :songs
 
-  root 'stations#index', as: 'sta'
+  root 'stations#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

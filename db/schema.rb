2795665e689a48d87dce4ba08e220b086df61893
Jess Ochs-Willard <jochs@brandeis.edu>
2015-03-18 16:28:21 -0400

    added upvotes/downvotes to review table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318192046) do
+ActiveRecord::Schema.define(version: 20150318202753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 20150318192046) do
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "upvotes"
+    t.integer  "downvotes"
   end
 
   create_table "songs", force: :cascade do |t|

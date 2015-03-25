f3aabc915037526e352b379f3bbc21b67f495e75
Jess Ochs-Willard <jochs@brandeis.edu>
2015-03-25 02:03:56 -0400

    modified station record to include has_many relation for albums, added a has_many relation for albums to artist as well

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323200513) do
+ActiveRecord::Schema.define(version: 20150325052314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 20150323200513) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "lastfm_id"
+  end
+
+  create_table "albums_stations", id: false, force: :cascade do |t|
+    t.integer "album_id"
+    t.integer "station_id"
   end
 
   create_table "artists", force: :cascade do |t|
@@ -80,7 +85,7 @@ ActiveRecord::Schema.define(version: 20150323200513) do
     t.string   "lastfm_id"
   end
 
-  create_table "station_albums", force: :cascade do |t|
+  create_table "station_albums", id: false, force: :cascade do |t|
     t.integer  "station_id"
     t.integer  "album_id"
     t.datetime "created_at", null: false

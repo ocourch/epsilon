408f9d207fd25ed20b55c8716877d2472d20e9c0
Oscar Courchaine <oscar.courchaine@gmail.com>
2015-04-09 15:22:48 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,23 +14,40 @@ Station.create(call_letters: "WBRS", location: "Waltham, MA", bio: "Test bio")
 
 s = Station.find(1)
 
+(1..6).each do |index|
+	f_name = Faker::Name.first_name
+	d_name = Faker::Name.title
+	l_name = Faker::Name.last_name
+	em = Faker::Internet.email
+	user = User.create! :email => em, :password => 'topsecret', :password_confirmation => 'topsecret', first_name: f_name, last_name: l_name,dj_alias: d_name, u_id: index
+	s.users << user
+end
+
+
+user1 = User.create! :email => 'oscar.courchaine@gmail.com', :password => '12345678', :password_confirmation => '12345678', first_name: 'Oscar', last_name: 'Courchaine' ,dj_alias: 'DJ Pamela', u_id: 100
+s.users << user1
+
+
 (1..1000).each do |index|
   artist_name = Faker::Name.name
 
   Artist.create(name: artist_name, genre: genres.sample, id: index, bio: Faker::Lorem.paragraph)
   
-  #Album.create(artist_id: index, name: Faker::Commerce.product_name, released: Faker::Date.between(10.years.ago, Time.now), in_lib?:tf.sample, id: index)
   
 
   s.albums << Album.create(genre: genres.sample, location: locations.sample, artist_id: index, name: Faker::Commerce.product_name, released: Faker::Date.between(10.years.ago, Time.now), in_lib?:tf.sample, id: index)
   
+   
+
   Song.create(title: Faker::Name.title, album_id: index, artist_id: index)
 
   StationAlbum.create(album_id: index)
 
   Song.create(title: Faker::Name.title, album_id: index, artist_id: index)
 
-  #StationAlbum.create(album_id: index)
+
 
 
 end
+
+

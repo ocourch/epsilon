25d4dab0d9ab0c8da75e69a7c5753b245e483e95
Oscar Courchaine <oscar.courchaine@gmail.com>
2015-04-22 14:26:14 -0400

    stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ tf = [true, false]
 
 Station.create(call_letters: "WBRS", location: "Waltham, MA", bio: "WBRS is the sole broadcasting entity at Brandeis University, dedicated to providing for the needs of its listeners in the areas of entertainment, information, and public service. WBRS also undertakes a wide variety of non-broadcasting services in order to diversify, improve, and advance broadcasting at Brandeis University. WBRS is the non-commercial student radio station licensed to Brandeis University in Waltham, Massachusetts. We broadcast 24/7/365 serving Brandeis University, Waltham and the surrounding communities. WBRS broadcasts with an ERP (effective radiated power) of 25 watts at an elevation of 46 meters HAAT (height above average terrain). Our facilities are located on the Brandeis campus with studios in the Shapiro Campus Center and transmitter on top of the Rabb Graduate Center. WBRS is licensed by the FCC as a Class D station, reaching Framingham and Natick to the west and as far as East Boston and Logan International Airport to the east.
 
-WBRS’ motto is ‘Something for Everyone.’ We encourage all artists, labels, and radio services to contribute material for airplay. WBRS prides itself in also being able to provide Brandeis University’s student body with a diverse array of services including radio DJ training and certification, concerts and live music events, equipment rental, station education, and professional development for students interested pursuing careers in media or broadcasting. Although WBRS is entirely student -run, the station also reaches out to the greater Waltham area with shows run by community members and Brandeis faculty. Tune in today for the latest and greatest from WBRS 100.1 FM!", id: '1')
+WBRS’ motto is ‘Something for Everyone.’ We encourage all artists, labels, and radio services to contribute material for airplay. WBRS prides itself in also being able to provide Brandeis University’s student body with a diverse array of services including radio DJ training and certification, concerts and live music events, equipment rental, station education, and professional development for students interested pursuing careers in media or broadcasting. Although WBRS is entirely student -run, the station also reaches out to the greater Waltham area with shows run by community members and Brandeis faculty. Tune in today for the latest and greatest from WBRS 100.1 FM!")
 
 
 
@@ -25,10 +25,6 @@ s = Station.find(1)
 	l_name = Faker::Name.last_name
 	em = Faker::Internet.email
 
-	user = User.create! :email => em, :password => 'topsecret', :password_confirmation => 'topsecret', first_name: f_name, last_name: l_name,dj_alias: d_name, id: index
-	#user.stations << s
-	s.users << user
-
 end
 
 
@@ -37,12 +33,12 @@ end
 (1..10).each do |index|
   artist_name = Faker::Name.name
 
-  Artist.create(name: artist_name, genre: genres.sample, id: index, bio: Faker::Lorem.paragraph)
+  Artist.create(name: artist_name, genre: genres.sample, bio: Faker::Lorem.paragraph)
   
   
 
 
-  s.albums << Album.create(genre: genres.sample, location: locations.sample, artist_id: index, name: Faker::Commerce.product_name, released: Faker::Date.between(10.years.ago, Time.now), in_lib?:tf.sample, id: index)
+  s.albums << Album.create(genre: genres.sample, location: locations.sample, artist_id: index, name: Faker::Commerce.product_name, released: Faker::Date.between(10.years.ago, Time.now), in_lib?:tf.sample)
   
    
 
@@ -54,22 +50,22 @@ end
 
 #adds Oscar as a user
 
-user1 = User.create! :site_admin => true, :email => 'oscar.courchaine@gmail.com', :password => '12345678', :password_confirmation => '12345678', first_name: 'Oscar', last_name: 'Courchaine' ,dj_alias: 'DJ Pamela', id: 100
+user1 = User.create! :site_admin => true, :email => 'oscar.courchaine@gmail.com', :password => '12345678', :password_confirmation => '12345678', first_name: 'Oscar', last_name: 'Courchaine' ,dj_alias: 'DJ Pamela'
 
-user2 = User.create! :site_admin => false, :email => 'jochs@brandeis.edu', :password => '12345678', :password_confirmation => '12345678', first_name: 'Jess', last_name: 'Ochs-Willard' ,dj_alias: 'DJ Flashy', id: 101
+user2 = User.create! :site_admin => false, :email => 'jochs@brandeis.edu', :password => '12345678', :password_confirmation => '12345678', first_name: 'Jess', last_name: 'Ochs-Willard' ,dj_alias: 'DJ Flashy'
 
 
 
 
 #gives oscar playlists
 (1..6).each do |index|
-	p = Playlist.create(title: Faker::Commerce.product_name, user_id: 100)
+	p = Playlist.create(title: Faker::Commerce.product_name, user_id: user1.id)
 	(1..6).each do |index|
 		r = Random.new
 		t = r.rand(150..350)
 		p.songs << Song.create(title: Faker::Name.title, album_id: index, artist_id: index, duration: t)
 	end
-	p1 = Playlist.create(title: Faker::Commerce.product_name, user_id: 101)
+	p1 = Playlist.create(title: Faker::Commerce.product_name, user_id: user2.id)
 	(1..6).each do |index|
 		r = Random.new
 		t = r.rand(150..350)

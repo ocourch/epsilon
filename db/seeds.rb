7ee1d88e80a4e41339ffc4ccd58842631405e603
Oscar Courchaine <oscar.courchaine@gmail.com>
2015-04-22 12:37:16 -0400

    lots of stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,9 @@ require 'faker'
 genres = ['Blues', 'Folk', 'Rock', 'Pop', 'Metal', 'Electronic', 'Jazz', 'Rap']
 locations = ['Spotify', 'mp3', 'Record: Shelf A', 'Record: Shelf B', 'Record: Shelf C', 'CD: Shelf D', 'CD: Shelf E', 'CD: Shelf F' ]
 tf = [true, false]
-Station.create(call_letters: "WBRS", location: "Waltham, MA", bio: "The Station of Brandeis University, committed to providing the best music for our fellow students", id: '1')
+Station.create(call_letters: "WBRS", location: "Waltham, MA", bio: "WBRS is the sole broadcasting entity at Brandeis University, dedicated to providing for the needs of its listeners in the areas of entertainment, information, and public service. WBRS also undertakes a wide variety of non-broadcasting services in order to diversify, improve, and advance broadcasting at Brandeis University. WBRS is the non-commercial student radio station licensed to Brandeis University in Waltham, Massachusetts. We broadcast 24/7/365 serving Brandeis University, Waltham and the surrounding communities. WBRS broadcasts with an ERP (effective radiated power) of 25 watts at an elevation of 46 meters HAAT (height above average terrain). Our facilities are located on the Brandeis campus with studios in the Shapiro Campus Center and transmitter on top of the Rabb Graduate Center. WBRS is licensed by the FCC as a Class D station, reaching Framingham and Natick to the west and as far as East Boston and Logan International Airport to the east.
+
+WBRS’ motto is ‘Something for Everyone.’ We encourage all artists, labels, and radio services to contribute material for airplay. WBRS prides itself in also being able to provide Brandeis University’s student body with a diverse array of services including radio DJ training and certification, concerts and live music events, equipment rental, station education, and professional development for students interested pursuing careers in media or broadcasting. Although WBRS is entirely student -run, the station also reaches out to the greater Waltham area with shows run by community members and Brandeis faculty. Tune in today for the latest and greatest from WBRS 100.1 FM!", id: '1')
 
 s = Station.find(1)
 #adds users to a station
@@ -46,8 +48,10 @@ end
 
 
 #adds Oscar as a user
-user1 = User.create! :email => 'oscar.courchaine@gmail.com', :password => '12345678', :password_confirmation => '12345678', first_name: 'Oscar', last_name: 'Courchaine' ,dj_alias: 'DJ Pamela', id: 100
-user1.stations << s
+user1 = User.create! :site_admin => true, :email => 'oscar.courchaine@gmail.com', :password => '12345678', :password_confirmation => '12345678', first_name: 'Oscar', last_name: 'Courchaine' ,dj_alias: 'DJ Pamela', id: 100
+
+user2 = User.create! :site_admin => false, :email => 'jochs@brandeis.edu', :password => '12345678', :password_confirmation => '12345678', first_name: 'Jess', last_name: 'Ochs-Willard' ,dj_alias: 'DJ Flashy', id: 101
+
 
 #gives oscar playlists
 (1..6).each do |index|
@@ -57,7 +61,15 @@ user1.stations << s
 		t = r.rand(150..350)
 		p.songs << Song.create(title: Faker::Name.title, album_id: index, artist_id: index, duration: t)
 	end
+	p1 = Playlist.create(title: Faker::Commerce.product_name, user_id: 101)
+	(1..6).each do |index|
+		r = Random.new
+		t = r.rand(150..350)
+		p.songs << Song.create(title: Faker::Name.title, album_id: index, artist_id: index, duration: t)
+	end
 	user1.playlists << p
+	user2.playlists << p1
 end
 
 s.users << user1
+s.users << user2

60515c7823aebbaa3e41cd1b6fae58a55bdacabe
Ethan Roseman <ethteck@brandeis.edu>
2015-05-04 18:13:42 -0400

    editing album page, link after adding album, stores release date automatically when available

--- app/controllers/last_fm_search_controller.rb
+++ app/controllers/last_fm_search_controller.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 class LastFmSearchController < ApplicationController
+  before_filter :authenticate_user!
 
   respond_to :json
 
@@ -16,14 +17,14 @@ class LastFmSearchController < ApplicationController
    album_snip = "?method=album.search&album="
    track_snip = "?method=track.search&track="
 
-   end_snip = params[:query] + limit_snip + "&api_key=949a97d4b7ab1cf6227269acd34f39ce&format=json"
+   end_snip = CGI.escape(params[:query].to_s) + limit_snip + "&api_key=949a97d4b7ab1cf6227269acd34f39ce&format=json"
 
    artist_search_URI = begin_snip + artist_snip + end_snip
    album_search_URI = begin_snip + album_snip + end_snip
    track_search_URI = begin_snip + track_snip + end_snip
 
    #artist_response = Net::HTTP.get_response(URI.parse(artist_search_URI))
-   album_response = Net::HTTP.get_response(URI.parse(album_search_URI.gsub(/ /, '+')))
+   album_response = Net::HTTP.get_response(URI.parse(album_search_URI))
    #track_response = Net::HTTP.get_response(URI.parse(track_search_URI))
 
    #artist_parsed_json = JSON.parse(artist_response.body)
@@ -35,6 +36,7 @@ class LastFmSearchController < ApplicationController
    #@tracks = track_parsed_json["results"]["trackmatches"]["track"]
 
    @query = params[:query]
+
  end
 
  def add_album
@@ -48,18 +50,31 @@ class LastFmSearchController < ApplicationController
    this_album = Album.find_or_create_by(artist_id: this_artist.id, name: album_name, image_url: album_image, image_url_large: album_image_large)
 
      # Add tracks for album
-     tracks_URI =  "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=949a97d4b7ab1cf6227269acd34f39ce&artist=" + artist_name + "&album=" + album_name + "&format=json"
-     tracks_response = Net::HTTP.get_response(URI.parse(tracks_URI.gsub(/ /, '+')))
+     tracks_URI =  "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=949a97d4b7ab1cf6227269acd34f39ce&artist=" + CGI.escape(artist_name) + "&album=" + CGI.escape(album_name) + "&format=json"
+     tracks_response = Net::HTTP.get_response(URI.parse(tracks_URI))
      tracks_json = JSON.parse(tracks_response.body)
 
      tracks = tracks_json["album"]["tracks"]["track"]
+
+     release_date = tracks_json["album"]["releasedate"]
+
+      # Stores release date
+     if (!release_date.empty?)
+        release = DateTime.parse(release_date = release_date.split(',').first)
+
+        this_album.released = release
+        this_album.save
+     end
+
+     #binding.pry
 
      tracks.each do |track|
       name = track["name"]
       duration = track["duration"]
       this_track = Song.find_or_create_by(title: name, duration: duration, artist_id: this_artist.id,album_id: this_album.id)
-
     end
+
+    @album_id = this_album.id
   end
 
   def run_tests
